539988dc485ea4ad606e4ae892d697ed8bc625a9
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-04-02 19:36:25 +0500

    categories models & categories admin created

---
+++ categories/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    image = models.ImageField(upload_to='category-images', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
