a21e607cf7ac9433f493afbeb3556e7e42c2bc11
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-04-02 22:05:03 +0500

    products models & products admin created

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from products.models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'discount_price', 'category', 'stock', 'is_active', 'created_at', 'updated_at')
+    search_fields = ('name', 'description', 'category')
--- products/models.py
+++ products/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from categories.models import Category
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=13, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=13, decimal_places=2, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    stock = models.PositiveIntegerField()
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, relatd_name='images')
+    image = models.ImageField(upload_to='product-images')
+    is_primary = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.product.name} image'
