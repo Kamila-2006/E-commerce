c9bdcbf37933fd6dcbd0c46d883da9ee57efac7d
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-04-03 18:54:05 +0500

    customers models & customers admin created

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'phone', 'address', 'created_at', 'updated_at')
+    search_fields = ('user', 'phone', 'address')
--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer')
+    phone = models.CharField(max_length=16, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.name
