c0962ee335e9e0b11731a7ce3bce5ad497f635bb
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-04-04 23:26:02 +0500

    products completed

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from products.models import Product
+from products.models import Product, ProductImage
 
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'discount_price', 'category', 'stock', 'is_active', 'created_at', 'updated_at')
     search_fields = ('name', 'description', 'category')
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image', 'is_primary')
--- products/serializers.py
+++ products/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import ProductImage, Product
-from django.db import transaction
 
 
 class ProductImageSerializer(serializers.ModelSerializer):
@@ -9,22 +8,11 @@ class ProductImageSerializer(serializers.ModelSerializer):
         fields = ('image', 'is_primary')
 
 class ProductSerializer(serializers.ModelSerializer):
-    images = ProductImageSerializer(many=True, read_only=True)  # ✅ Read uchun
-    uploaded_images = serializers.ListField(
-        child=serializers.ImageField(), write_only=True  # ✅ Write uchun
-    )
+    images = ProductImageSerializer(many=True)
     category_name = serializers.CharField(source='category.name', read_only=True)
     category_by_name = serializers.CharField(source='category.name', read_only=True)
 
     class Meta:
         model = Product
-        fields = ('id', 'name', 'description', 'price', 'discount_price', 'category', 'category_name', 'category_by_name', 'stock', 'is_active', 'created_at', 'updated_at', 'images', 'uploaded_images')
+        fields = ('id', 'name', 'description', 'price', 'discount_price', 'category', 'category_name', 'category_by_name', 'stock', 'is_active', 'created_at', 'updated_at', 'images')
         read_only_fields = ('id', 'created_at', 'updated_at')
-
-    def create(self, validated_data):
-        images_data = validated_data.pop('uploaded_images', [])  # ✅ To‘g‘ri maydon
-        with transaction.atomic():
-            product = Product.objects.create(**validated_data)
-            for image in images_data:
-                ProductImage.objects.create(product=product, image=image)
-        return product
