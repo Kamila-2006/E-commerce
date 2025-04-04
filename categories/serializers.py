1d5ea7c63aaee3f65de90742097e7d2ba46b33f0
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-04-04 15:14:44 +0500

    categories completed

--- categories/serializers.py
+++ categories/serializers.py
@@ -5,5 +5,5 @@ from .models import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name', 'description', 'image', 'created_at', 'updated_at']
+        fields = ['id', 'name', 'description', 'parent', 'image', 'created_at', 'updated_at']
         read_only_fields = ['id', 'created_at', 'updated_at']
