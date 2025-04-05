fa1d15ccff988a1b0a1f0955e81c03cd1b9b4472
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-04-05 19:46:11 +0500

    categories completed

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
+from products.models import Product
 from .models import Category
 
 
+class CategoryProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'price')
+
 class CategorySerializer(serializers.ModelSerializer):
     parent_name = serializers.CharField(source='parent.name', read_only=True, required=False)
+    products = CategoryProductSerializer(many=True)
+    products_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Category
-        fields = ['id', 'name', 'description', 'parent', 'parent_name', 'image', 'created_at', 'updated_at']
-        read_only_fields = ['id', 'created_at', 'updated_at']
+        fields = ['id', 'name', 'description', 'parent', 'parent_name', 'image', 'products', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at', 'products']
