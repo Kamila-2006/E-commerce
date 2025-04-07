7a17d508656888e0835d991591aaf7fab77ccc53
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-04-07 19:05:31 +0500

    customer-orders list created

--- customers/urls.py
+++ customers/urls.py
@@ -8,4 +8,5 @@ router.register(r'customers', views.CustomerViewSet, basename='customers')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('customers/<int:pk>/orders/', views.CustomerOrdersView.as_view(), name='customer-orders'),
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,10 +1,22 @@
 from rest_framework import viewsets
+
+from orders.serializers import OrderSerializer
 from .models import Customer
 from .serializers import CustomerSerializer
 from .pagination import CustomerPagination
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     pagination_class = CustomerPagination
+
+class CustomerOrdersView(APIView):
+    def get(self, request, pk):
+        customer = get_object_or_404(Customer, pk=pk)
+        orders = customer.orders.all()
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data, status=200)
